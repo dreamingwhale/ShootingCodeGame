c2b42e7581d13df9207c9d71dc434c857c8ea983
dreamingwhale <dreamingskywhale@gmail.com>
2024-01-03 17:49:59 +0900

    딜리게이트

--- Source/L20231220_Recap/L20231220_Recap.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "L20231220_Recap.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, L20231220_Recap, "L20231220_Recap" );
- 
